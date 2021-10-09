641e6a7f2dea4bed611deb9c918687843a1b4f7a
saurin thakkar <saurin.thakkar13@gmail.com>
2021-10-09 23:55:17 +0530

    added cartitem component and its styles

--- cart-app/src/App.js
+++ cart-app/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import CartItem from "./CartItem";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <CartItem/>
     </div>
   );
 }
